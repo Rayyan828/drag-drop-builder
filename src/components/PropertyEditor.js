661c99bfe829eeb98cf7859087d421a8d37dcf7b
Rayyan828 <cs22b2001@iiitdm.ac.in>
2025-04-22 21:10:04 +0530

    Create PropertyEditor.js

---
+++ src/components/PropertyEditor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PropertyEditor = () => {
+  return (
+    <div className="property-editor">
+      <h2>Edit Properties</h2>
+      {/* Add form elements to edit properties of items */}
+    </div>
+  );
+};
