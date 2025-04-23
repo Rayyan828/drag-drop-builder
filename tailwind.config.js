502ee64d290d4e13c10958cc563d8099c9996dcc
Rayyan828 <cs22b2001@iiitdm.ac.in>
2025-04-23 14:22:21 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      // You can add custom colors, fonts, spacing, etc. here
+    },
+  },
+  plugins: [
+    // e.g. require('@tailwindcss/forms'),
+  ],
+};
